23b38c09e76b1a560823a51e16f89d75c56ad41f
Mahmoud Sharara <mahmoodshrara@gmail.com>
2024-07-24 20:44:40 +0300

    adding frist task

---
+++ Week One/Task1/README.md
@@ -0,0 +1,7 @@
+use IaC Terrafrom to build the following resource besides requirement specifications:
+
+* Create VPC.
+* Create Public subnet.
+* Create internet gateway attached to the subent.
+* Create route table to route all trrafic to the internet gateway.
+
---
+++ Week One/Task1/main.tf
@@ -0,0 +1,20 @@
+
+# Configure the AWS Provider
+provider "aws" {
+  region  = "us-east-1"
+  profile = "default"
+}
+
+# create AWS-vpc 
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+ 
+
+}
+
+#create AWS-subnet
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.10.0/24"
+  availability_zone = "us-east-1a"
+}
