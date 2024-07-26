7951f10d0696e1d83c1147daaa8f4889f8376562
Mahmoud Sharara <mahmoodshrara@gmail.com>
2024-07-26 15:47:12 +0300

    adding some modified resourses in task2

--- Week One/Task2/main.tf
+++ Week One/Task2/main.tf
@@ -6,31 +6,44 @@ provider "aws" {
 }
 
 
-# create variables
+# Create variables for environment and owner
 variable "Environment" {}
 variable "Owner" {}
 
 
+
+# Create an S3 bucket resource
 resource "aws_s3_bucket" "bucket1" {
-  bucket = "bucket1"
+  bucket = "s3-bucket-state-file"
+  force_destroy = true
+  object_lock_enabled = false
 
   tags = {
-  Name        = "bucket1"
+  Name        = "erakiterrafromstatefiles"
   Environment = var.Environment
   Owner       = var.Owner
   }
 }
 
-resource "aws_s3_bucket_acl" "bucket_acl" {
-  bucket = aws_s3_bucket.bucket1.id
-  acl    = "private"
-  
-}
-
-resource "aws_s3_bucket_versioning" "versioning1" {
+# Enable versioning for the S3 bucket
+resource "aws_s3_bucket_versioning" "versioning_bucket1" {
   bucket = aws_s3_bucket.bucket1.id
   versioning_configuration {
     status = "Enabled"
   }
+  
 }
+
+# Configure ownership controls for the S3 bucket
+resource "aws_s3_bucket_ownership_controls" "control1" {
+  bucket = aws_s3_bucket.bucket1.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+
+
+
 
