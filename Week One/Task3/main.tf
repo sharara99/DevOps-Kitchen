64ead035f85b03994bf1f73c4e646d21c8e758dd
Mahmoud Sharara <mahmoodshrara@gmail.com>
2024-07-28 16:45:22 +0300

    adding the edit on resources

--- Week One/Task3/main.tf
+++ Week One/Task3/main.tf
@@ -1,22 +1,13 @@
 
-# Configure the AWS Provider
-provider "aws" {
-  region  = "us-east-1"
-  profile = "default"
-}
-
-# Create variables for environment and owner
-variable "Environment" {}
-variable "Owner" {}
-
 # Create an S3 bucket resource
 resource "aws_s3_bucket" "bucket1" {
-  bucket              = "s3-sharara-dev"
+  bucket = "s3-sharara-task3"
+
   force_destroy       = true
   object_lock_enabled = false
 
   tags = {
-    Name        = "erakiterrafromstatefiles"
+    Name        = "s3-sharara-task3"
     Environment = var.Environment
     Owner       = var.Owner
   }
@@ -27,15 +18,6 @@ resource "aws_s3_bucket_versioning" "versioning_bucket1" {
   bucket = aws_s3_bucket.bucket1.id
   versioning_configuration {
     status = "Enabled"
-  }
-}
-
-# Configure ownership controls for the S3 bucket
-resource "aws_s3_bucket_ownership_controls" "control1" {
-  bucket = aws_s3_bucket.bucket1.id
-
-  rule {
-    object_ownership = "BucketOwnerPreferred"
   }
 }
 
--- Week One/Task3/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-Environment = "terraformChamps"
-Owner       = "Mahmoud Sharara"
