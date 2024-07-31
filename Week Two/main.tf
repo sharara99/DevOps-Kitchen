3074955b080cb5dfe5a67d1a53cfdb7939b631a3
Mahmoud Sharara <mahmoodshrara@gmail.com>
2024-07-31 09:17:23 +0300

    Adding the main task of week two

---
+++ Week Two/main.tf
@@ -0,0 +1,59 @@
+# Create S3 bucket with specified name, force_destroy set to true, and tags
+resource "aws_s3_bucket" "bucket1" {
+  bucket              = "s3-sharara-task5"
+  force_destroy       = true
+  object_lock_enabled = false
+
+  tags = {
+    Name        = "s3-sharara-task5"
+    Environment = var.Environment
+    Owner       = var.Owner
+  }
+}
+
+# Block all public access at the bucket level
+resource "aws_s3_bucket_public_access_block" "block_all" {
+  bucket = aws_s3_bucket.bucket1.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# Enable versioning on the S3 bucket
+resource "aws_s3_bucket_versioning" "versioning_bucket1" {
+  bucket = aws_s3_bucket.bucket1.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Configure objects encryption as "SSE-S3" and enable bucket key
+resource "aws_s3_bucket_server_side_encryption_configuration" "sse" {
+  bucket = aws_s3_bucket.bucket1.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+
+    bucket_key_enabled = true
+  }
+}
+
+# Configure lifecycle rule to expire objects after 7 days
+resource "aws_s3_bucket_lifecycle_configuration" "lifecycle" {
+  bucket = aws_s3_bucket.bucket1.id
+
+  rule {
+    id     = "expire-objects"
+    status = "Enabled"
+
+    expiration {
+      days = 7
+    }
+
+  }
+}
---
+++ Week Two/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.58.0"
+    }
+  }
+  backend "s3" {
+    bucket = "s3-sharara-task5"
+    key    = "bucket1.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "default"
+}
