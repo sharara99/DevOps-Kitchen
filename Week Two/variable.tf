f7475161de6d022c2e01b8087ce61bdf87062f52
Mahmoud Sharara <mahmoodshrara@gmail.com>
2024-08-01 13:40:14 +0300

    editing variables

--- Week Two/variable.tf
+++ Week Two/variable.tf
@@ -1,9 +1,10 @@
 # Create variables for environment and owner
 variable "Environment" {
   description = "The environment for the S3 bucket"
-  default     = "terraformChamps"
+  type        = string
 }
+
 variable "Owner" {
   description = "The owner of the S3 bucket"
-  default     = "Mahmoud Sharara"
+  type        = string
 }
