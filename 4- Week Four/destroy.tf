7ef53d4b3d453cc2ae9216f43014ef3875c3fdef
Mahmoud Sharara <mahmoodshrara@gmail.com>
2024-08-08 18:48:46 +0300

    addin the complete week4

--- 4- Week Four/destroy.tf
+++ 4- Week Four/destroy.tf
@@ -7,5 +7,5 @@ resource "null_resource" "cleanup" {
     EOT
   }
 
-  depends_on = [aws_s3_bucket.bucket1, aws_instance.ubuntu-instance, aws_key_pair.UbuntuKP]
+  depends_on = [aws_s3_bucket.bucket1, aws_instance.ubuntu-instance, aws_key_pair.UbuntuKP, aws_iam_role.ec2_role,aws_iam_role_policy_attachment.ec2_role_policy_attachment]
 }
