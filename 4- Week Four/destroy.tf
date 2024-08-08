c6811ecb3071549ea11211589953840f23fd5992
Mahmoud Sharara <mahmoodshrara@gmail.com>
2024-08-08 19:32:38 +0300

    addin the complete week4

--- 4- Week Four/destroy.tf
+++ 4- Week Four/destroy.tf
@@ -12,5 +12,7 @@ resource "null_resource" "cleanup" {
                 aws_key_pair.UbuntuKP, 
                 aws_iam_role.ec2_role,
                 aws_iam_role_policy_attachment.ec2_role_policy_attachment,
-                awaws_security_group.UbuntuSG]
+                aws_security_group.UbuntuSG,
+                aws_iam_instance_profile.ec2_instance_profile
+                ]
 }
