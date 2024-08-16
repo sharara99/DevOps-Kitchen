bc8b8dfbe184faf6841afe8eed806fa79acc2992
Mahmoud Sharara <mahmoodshrara@gmail.com>
2024-08-17 01:56:34 +0300

    adding the backend

--- 5- Week Five/provider.tf
+++ 5- Week Five/provider.tf
@@ -5,7 +5,11 @@ terraform {
       version = "~> 5.58.0"
     }
   }
-
+  backend "s3" {
+    bucket = "s3-sharara-week5"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
