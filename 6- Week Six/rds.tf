3a39e9423abbcdb8bb9528d622a9f675d80def78
Mahmoud Sharara <mahmoodshrara@gmail.com>
2024-08-22 13:11:42 +0300

    adding week 6

--- 6- Week Six/rds.tf
+++ 6- Week Six/rds.tf
@@ -12,19 +12,19 @@ resource "aws_db_subnet_group" "rds_subnet_group" {
 
 # Create a PostgreSQL RDS instance
 resource "aws_db_instance" "postgres" {
-  allocated_storage    = 20
-  engine               = "postgres"
-  engine_version       = "16.4"
-  instance_class       = "db.t3.micro"
-  db_name              = "postgresqldatabase"
-  username             = "postgres"
-  identifier           = "postgres-rds"
-  password             = var.db_password
-  db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
+  allocated_storage      = 20
+  engine                 = "postgres"
+  engine_version         = "16.4"
+  instance_class         = "db.t3.micro"
+  db_name                = "postgresqldatabase"
+  username               = "postgres"
+  identifier             = "postgres-rds"
+  password               = var.db_password
+  db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
   vpc_security_group_ids = [aws_security_group.sg_01.id]
-  skip_final_snapshot  = true
-  multi_az             = true
-  publicly_accessible  = false
+  skip_final_snapshot    = true
+  multi_az               = true
+  publicly_accessible    = false
 
   tags = {
     Name        = "postgres-rds"
--- 6- Week Six/terraform.tfvars
+++ 6- Week Six/terraform.tfvars
@@ -1,7 +1,7 @@
 # This file contains variable values 
-Environment  = "terraformChamps"
-Owner        = "Mahmoud Sharara"
-vpc_cidr     = "10.0.0.0/16"
+Environment    = "terraformChamps"
+Owner          = "Mahmoud Sharara"
+vpc_cidr       = "10.0.0.0/16"
 subnet_01_cidr = "10.0.1.0/24"
 subnet_02_cidr = "10.0.2.0/24"
-db_password  = "Admin-1234"
+db_password    = "Admin-1234"
