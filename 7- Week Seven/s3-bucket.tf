57a13e7cce67ff2f4419c42f016c134b48e87c34
Mahmoud Sharara <mahmoodshrara@gmail.com>
2024-08-24 00:39:53 +0300

    adding week 7 basic files

--- 7- Week Seven/s3-bucket.tf
+++ 7- Week Seven/s3-bucket.tf
@@ -1,6 +1,6 @@
 # Create an S3 bucket resource
 resource "aws_s3_bucket" "bucket1" {
-  bucket = "s3-sharara-week6"
+  bucket = "s3-sharara-week7"
 
   force_destroy       = true
   object_lock_enabled = false
