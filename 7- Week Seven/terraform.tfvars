c6ca191336ff5b6816c52c074b796ca9188ce666
Mahmoud Sharara <mahmoodshrara@gmail.com>
2024-08-24 10:54:28 +0300

    delete all files

--- 7- Week Seven/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-# This file contains variable values 
-Environment           = "terraformChamps"
-Owner                 = "Mahmoud Sharara"
--- 7- Week Seven/variable.tf
+++
@@ -1,9 +0,0 @@
-variable "Environment" {
-  description = "The environment for the resources"
-  type        = string
-}
-
-variable "Owner" {
-  description = "The owner of the S3 bucket"
-  type        = string
-}
